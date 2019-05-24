3b16b919295a57bc2daa0b48bc80b4c1ea5fcd6e
Mike McMahon <mmm219@mcc.northwestern.edu>
2019-05-23 20:10:13 -0500

    added questions to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,31 @@
+var questionAnswer = [
+    {
+        question: "What medical condition did the character Walter White Jr. suffer from on the TV Show 'Breaking Bad'?",
+        answers: ["Cerebral Palsy", "Parkinson's Disease", "Multiple Sclerosis", "Cancer"],
+        correctAnswer: "Cerebral Palsy",
+        animate: "TEST",
+    },
+    {
+        question: "What was the name of the original advertising agency character 'Don Draper' worked for on the TV Show 'Mad Men'?",
+        answers: ["Sterling Cooper", "Olson Pryce", "Campbell Cosgrove", "Crane & Associates"],
+        correctAnswer: "Sterling Cooper",
+        animate: "TEST",
+    },
+    {
+        question: "What is the female lead character of the TV Show 'Parks and Recreation'?",
+        answers: ["Liz Lemon", "Leslie Knope", "Miranda Sings", "Stefanie Wilsack"],
+        correctAnswer: "Leslie Knope",
+        animate: "TEST",
+    },
+    {
+        question: "What mythical lands do the 9 great houses fight over in the TV Show 'The Game of Thrones'?",
+        answers: ["Valhalla", "Westeros", "Avalon", "Middle Earth"],
+        correctAnswer: "Westeros",
+        animate: "TEST",
+    }
+
+]
+
 
 //  Set our number counter to 30 and initialize variables.
 var timer = 30;
@@ -19,19 +47,14 @@ function questionDisplay() {
 
 //  The decrement function.
 function decrement() {
-
     //  Decrease timer by one.
     timer--;
-
     //  Show the timer in the #showNumber tag.
     $("#showNumber").html("<h2>" + "Time Remaining: " + timer + "</h2>");
-
     //  Once timer hits zero...
     if (timer === 0) {
-
         //  ...run the stop function.
         stop();
-
         //  Alert the user that time is up.
         // alert("Time Up!");
     }
