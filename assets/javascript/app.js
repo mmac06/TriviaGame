c392f388f3e55eeee8546666b46d4694778180bc
Mike McMahon <mmm219@mcc.northwestern.edu>
2019-05-24 20:22:43 -0500

    SUCCESSFULLY corrected buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,25 +3,25 @@ var questionAnswer = [
     {
         question: "What medical condition did the character Walter White Jr. suffer from on the TV Show 'Breaking Bad'?",
         answers: ["Cerebral Palsy", "Parkinson's Disease", "Multiple Sclerosis", "Cancer"],
-        correctAnswer: "Cerebral Palsy",
+        correctAnswer: 1,
         animate: "TEST",
     },
     {
         question: "What was the name of the original advertising agency character 'Don Draper' worked for on the TV Show 'Mad Men'?",
         answers: ["Sterling Cooper", "Olson Pryce", "Campbell Cosgrove", "Crane & Associates"],
-        correctAnswer: "Sterling Cooper",
+        correctAnswer: 1,
         animate: "TEST",
     },
     {
         question: "What is the female lead character of the TV Show 'Parks and Recreation'?",
         answers: ["Liz Lemon", "Leslie Knope", "Miranda Sings", "Stefanie Wilsack"],
-        correctAnswer: "Leslie Knope",
+        correctAnswer: 2,
         animate: "TEST",
     },
     {
         question: "What mythical lands do the 9 great houses fight over in the TV Show 'The Game of Thrones'?",
         answers: ["Valhalla", "Westeros", "Avalon", "Middle Earth"],
-        correctAnswer: "Westeros",
+        correctAnswer: 2,
         animate: "TEST",
     }
 
@@ -34,6 +34,7 @@ var scoreCorrect = 0;
 var scoreIncorrect = 0;
 var scoreUnanswered = 0;
 var index = 0;
+var userGuess = "";
 var intervalId;
 var question;
 
@@ -46,16 +47,30 @@ function run() {
     intervalId = setInterval(decrement, 1000);
 }
 
+// for (var i = 0; i < questionAnswer[index].answers.length; i++) {
+//     var buttonID = $("<button>")
+//     // questionAnswer[index].answers[i];
+//     buttonID.attr("id", i);
+//     console.log(buttonID);
+// }
+
+
+
 // display questions and answers
 function triviaDisplay() {
     $("#questionBox").html("<h4>" + questionAnswer[index].question + "</h4>");
-    for (var i = 0; i < questionAnswer[index].answers.length; i++){
-    $("#answerBox").append(" " + "<button>" + questionAnswer[index].answers[i] + "</button>");
+    for (var i = 0; i < questionAnswer[index].answers.length; i++) {
+        var button = $("<button>");
+        button.attr("id", "buttonValue" + i);
+        console.log(button);
+        $("#answerBox").append(button);
     }
-    $("button").click(function () {
-        alert("You clicked a button!");
-        
-        });
+    for (var i = 0; i < questionAnswer[index].answers.length; i++){
+        $("#buttonValue"+[i]).append(questionAnswer[index].answers[i]);
+    }
+    
+
+
 
 }
 
