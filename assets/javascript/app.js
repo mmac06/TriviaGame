079859e66487e887ed532753958215bf2eb7391c
Mike McMahon <mikemcmahon@host1841693575.direcway.com>
2019-05-28 16:34:09 -0400

    remove old folders, fix timer appear

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,154 @@
+//  Set our number counter to 30 and initialize variables.
+var timer = 30;
+var scoreCorrect = 0;
+var scoreIncorrect = 0;
+var currentQuestion = 0;
+var userChoice = "";
+var intervalId;
+var question;
+
 // Set up question/answer array
 var questionAnswer = [
     {
         question: "What medical condition did the character Walter White Jr. suffer from on the TV Show 'Breaking Bad'?",
         answers: ["Cerebral Palsy", "Parkinson's Disease", "Multiple Sclerosis", "Cancer"],
         correctAnswer: "Cancer",
+        // correctAnswer: 3,
         animate: "/TriviaGame/assets/images/lizlemon1.gif",
+        wrongAnimate: "/TriviaGame/assets/images/wrong1.gif",
     },
     {
         question: "What was the name of the original advertising agency character 'Don Draper' worked for on the TV Show 'Mad Men'?",
         answers: ["Sterling Cooper", "Olson Pryce", "Campbell Cosgrove", "Crane & Associates"],
         correctAnswer: "Sterling Cooper",
-        animate: "TEST",
+        // correctAnswer: 0,
+        animate: "/TriviaGame/assets/images/lizlemon1.gif",
+        wrongAnimate: "/TriviaGame/assets/images/wrong1.gif",
     },
     {
         question: "What is the female lead character of the TV Show 'Parks and Recreation'?",
         answers: ["Liz Lemon", "Leslie Knope", "Miranda Sings", "Stefanie Wilsack"],
         correctAnswer: "Leslie Knope",
-        animate: "TEST",
+        // correctAnswer: 1,
+        animate: "/TriviaGame/assets/images/lizlemon1.gif",
+        wrongAnimate: "/TriviaGame/assets/images/wrong1.gif",
     },
     {
         question: "What mythical lands do the 9 great houses fight over in the TV Show 'The Game of Thrones'?",
         answers: ["Valhalla", "Westeros", "Avalon", "Middle Earth"],
         correctAnswer: "Westeros",
-        animate: "TEST",
+        // correctAnswer: 1,
+        animate: "/TriviaGame/assets/images/lizlemon1.gif",
+        wrongAnimate: "/TriviaGame/assets/images/wrong1.gif",
     }
 
 ]
 
+$(document).ready(function () {
 
-//  Set our number counter to 30 and initialize variables.
-var timer = 30;
-var scoreCorrect = 0;
-var scoreIncorrect = 0;
-var scoreUnanswered = 0;
-var index = 0;
-var userChoice = "";
-var intervalId;
-var question;
+    $(".start a").click(function (e) {
+        e.preventDefault();
+        $(".start").hide();
+        $(".quiz").show();
+        showQuestion();
+        runTimer();
+
+    });
+
+
+    // WHEN USER SELECTS BUTTON ANSWERS
+    $(".quiz ul").on("click", "button", function () {
+        $(".selected").removeClass("selected");
+        $(this).addClass("selected");
+        if ($("button.selected").length) {
+            userChoice = $("button.selected").text();
+            console.log(userChoice);
+            // passes the variable "userChoice" into the checkAnswer function below
+            checkAnswer(userChoice);
+        };
+    });
+
+    $(".summary a").click(function (e) {
+        e.preventDefault();
+        reset();
+    });
+
+});
+
+// calls the question and answer options
+function showQuestion() {
+    var question = questionAnswer[currentQuestion];
+    $("#questionBox").text(question.question);
+    $(".quiz ul").empty();
+    // adds answers with ID based on position in array
+    for (var i = 0; i < question.answers.length; i++) {
+        // $(".quiz ul").append("<button id = " + i + ">" + question.answers[i] + "</button>")
+        $(".quiz ul").append("<button id = " + i + " type = button class = \"btn btn-dark\">" + question.answers[i] + "</button>")
+    }
+
+}
+
+// checks the user selection vs. the correct answer from the array
+function checkAnswer(userChoice) {
+    var question = questionAnswer[currentQuestion];
+    if (question.correctAnswer === userChoice) {
+        scoreCorrect++;
+        stop();
+        alert("Correct!");
+        // creates gif image and hides after 3 seconds
+        $("#imageBox").html("<img src=" + questionAnswer[currentQuestion].animate + ">").show();
+        setTimeout(function () { $("#imageBox").hide(); }, 3000);
+
+    } else {
+        scoreIncorrect++;
+        stop();
+        alert("Incorrect! The correct answer is: " + questionAnswer[currentQuestion].correctAnswer);
+        $("#imageBox").html("<img src=" + questionAnswer[currentQuestion].wrongAnimate + ">").show();
+        setTimeout(function () { $("#imageBox").hide(); }, 3000);
+
+    }
+   
+    
+    // hides question/answers while gif displays, then calls the next question/answer series
+    timer = 30;
+    $("#questionBox").hide();
+    $("#answerBox").hide();
+    $("#timer").hide();
+    currentQuestion++;
+
+    setTimeout(function () {$("#questionBox").show()}, 3000);
+    setTimeout(function () {$("#answerBox").show()}, 3000);
+    setTimeout(function () {$("#timer").show()}, 3000);
+    setTimeout(function () { runTimer() }, 2700);
+
+
+
+    // pulls summary screen after user answers all questions
+    if (currentQuestion >= questionAnswer.length) {
+        showSummary();
+    } else {
+        showQuestion();
+    }
+}
+
+// shows score at end of game
+function showSummary() {
+    $(".quiz").hide();
+    $(".summary").show();
+    $("#scoreCorrect").text("Correct Answers: " + scoreCorrect);
+    $("#scoreIncorrect").text("Incorrect Answers: " + scoreIncorrect);
+
+}
+
+function reset() {
+    timer = 30;
+    scoreCorrect = 0;
+    scoreIncorrect = 0;
+    currentQuestion = 0;
+    $(".start").show();
+    $(".summary").hide();
+}
+
 
 //  The run function sets an interval
 //  that runs the decrement function once a second.
@@ -48,64 +160,28 @@ function runTimer() {
 }
 
 
-// display questions and answers
-function triviaDisplay() {
-    $("#questionBox").html("<h4>" + questionAnswer[index].question + "</h4>");
-    for (var i = 0; i < questionAnswer[index].answers.length; i++) {
-        var button = $("<button>");
-        button.attr("id", "buttonValue" + i);
-        console.log(button);
-        $("#answerBox").append(button);
-        // console.log("test: " + questionAnswer[index ++].question);
-    }
-    for (var i = 0; i < questionAnswer[index].answers.length; i++) {
-        $("#buttonValue" + [i]).append(questionAnswer[index].answers[i]);
 
-    }
-
-    for (var i = 0; i < questionAnswer[index].answers.length; i++) {
-        $("#buttonValue" + [i]).click(function () {
-            userChoice = $(this).text();
-            console.log(userChoice);
-
-            // if user guesses correctly vs incorrectly
-            if (userChoice === questionAnswer[index].correctAnswer) {
-                scoreCorrect ++;
-                alert("Correct!");
-                $("#imageBox").html("<img src=" + questionAnswer[index].animate + ">");
-                stop(timer);
-
-
-            } else {
-                scoreIncorrect++;
-                alert("Incorrect! The correct answer is: " + questionAnswer[index].correctAnswer);
-                stop(timer);
-            }
-
-        });
-
-
-    }
-
-}
-
-
-console.log("choice: " + userChoice);
-console.log("questionAnswer[index].question: " + questionAnswer[3].question)
 
 //  The decrement function.
 function decrement() {
     //  Decrease timer by one.
     timer--;
     //  Show the timer in the #showNumber tag.
-    $("#showNumber").html("<h2>" + "Time Remaining: " + timer + "</h2>");
+    $("#timer").html("<h2>" + "Time Remaining: " + timer + "</h2>");
     //  Once timer hits zero...
     if (timer === 0) {
-        scoreUnanswered ++;
-        //  ...run the stop function.
-        stop();
+        scoreIncorrect++;
         //  Alert the user that time is up.
-        // alert("Time Up!");
+        alert("Time Up!");
+        timer = 30;
+        currentQuestion++;
+        if (currentQuestion >= questionAnswer.length) {
+            stop();
+            showSummary();
+        } else {
+            showQuestion();
+
+        }
     }
 }
 function stop() {
@@ -114,8 +190,3 @@ function stop() {
     //  to the clearInterval function.
     clearInterval(intervalId);
 }
-
-// Run my functions
-runTimer();
-triviaDisplay();
-// answerDisplay();
